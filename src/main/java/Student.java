a216fe1c9bd6b78903c4bba787b7c4f15862cb71
Nowu <blingbling_cj@163.com>
2020-06-28 15:51:23 +0800

    update

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,6 @@
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java!");
+        System.out.println("I'm a student.");
     }
 }
