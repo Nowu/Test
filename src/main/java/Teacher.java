7cb0a695811d3dad43de47919e7fb5836de6c12e
Nowu <blingbling_cj@163.com>
2020-06-28 17:59:20 +0800

    Update Teacher.java

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,6 +1,9 @@
 public class Teacher {
+    private name;
+    private age;
     public static void main(String[] args) {
         System.out.println("I'm a teacher.");
         System.out.println("I'm a gootd teacher.");
+        
     }
 }
