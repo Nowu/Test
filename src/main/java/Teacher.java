e9778ae46dbd8f4a5eff202808a333744ca75af3
Nowu <blingbling_cj@163.com>
2020-06-28 18:06:21 +0800

    Update Teacher.java

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -4,6 +4,7 @@ public class Teacher {
     public static void main(String[] args) {
         System.out.println("I'm a teacher.");
         System.out.println("I'm a gootd teacher.");
+        System.out.println("I'm a gootd teacher.-master");
         
     }
 }
